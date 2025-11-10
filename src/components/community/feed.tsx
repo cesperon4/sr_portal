b822d95288c3c819db5681f6171778b708c60cb3
cesperon <cesperon4@gmail.com>
2025-11-09 23:18:20 -0800

    hot fix

--- src/components/community/feed.tsx
+++ src/components/community/feed.tsx
@@ -10,8 +10,7 @@ function Feed() {
   const LIMIT = 5;
   const { data, loading, error, fetchMore } = useGetPostsQuery({
     variables: { data: { limit: LIMIT, cursor: null } },
-    fetchPolicy: "cache-and-network",
-
+    fetchPolicy: "cache-first", // only fetch if cache is empty
   }); //refetch the posts using refetch { data, loading, error, refetch}}
 
   const posts = useMemo(() => data?.posts.posts ?? [], [data]); //useMemo ensures that posts only changes when data changes, not on every render.
