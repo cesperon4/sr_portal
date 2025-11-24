0e80e31b7caedf343854649dde92b8ac1f9a0422
cesperon <cesperon4@gmail.com>
2025-11-24 14:54:25 -0800

    hot fix

--- src/components/map/map-modal.tsx
+++ src/components/map/map-modal.tsx
@@ -23,28 +23,31 @@ export function MapModal({ incident, closeMapModal }: MapModalProps) {
         </h2>
 
         <div className="grid grid-cols-3 gap-4">
-          <Field label="Agency" value={a.Agency} />
-          <Field label="Date Occurred" value={a.DateOccurred} />
-          <Field label="Charge Count" value={a.ChargeCount} />
-          <Field label="Day Of the Week" value={a.DayOfWeek} />
-          <Field label="Hour Occurred" value={a.HourOccurred} />
-          <Field label="Beat Zone" value={a.Beat_Zone} />
-          <Field label="City" value={a.City} />
-          <Field label="State" value={a.State} />
+          <Field label="Agency" value={a.Agency || ""} />
+          <Field label="Date Occurred" value={a?.DateOccurred || ""} />
+          <Field label="Charge Count" value={a?.ChargeCount || ""} />
+          <Field label="Day Of the Week" value={a.DayOfWeek || ""} />
+          <Field label="Hour Occurred" value={a?.HourOccurred || ""} />
+          <Field label="Beat Zone" value={a?.Beat_Zone || ""} />
+          <Field label="City" value={a?.City || ""} />
+          <Field label="State" value={a?.State || ""} />
 
           {/* Repeated Beat Zone fields preserved */}
 
-          <Field label="Incident Number" value={a.Incident_number} />
+          <Field label="Incident Number" value={a?.Incident_number || ""} />
           <Field label="Latitude" value={a.LAT} />
           <Field label="Longitude" value={a.LON} />
-          <Field label="Location Type" value={a.Location_type} />
-          <Field label="Month Stamp" value={a.MonthStamp} />
-          <Field label="Year" value={a.YearStamp} />
-          <Field label="Part I" value={a.PartI} />
-          <Field label="Statute" value={a.Statute} />
-          <Field label="Statute Description" value={a.StatuteDescription} />
-          <Field label="Street" value={a.Street} />
-          <Field label="Zip Code" value={a.ZIP} />
+          <Field label="Location Type" value={a?.Location_type || ""} />
+          <Field label="Month Stamp" value={a?.MonthStamp || ""} />
+          <Field label="Year" value={a?.YearStamp || ""} />
+          <Field label="Part I" value={a?.PartI || ""} />
+          <Field label="Statute" value={a?.Statute || ""} />
+          <Field
+            label="Statute Description"
+            value={a?.StatuteDescription || ""}
+          />
+          <Field label="Street" value={a?.Street || ""} />
+          <Field label="Zip Code" value={a?.ZIP || ""} />
         </div>
 
         <div className="pt-4 flex justify-end">
@@ -61,7 +64,7 @@ export function MapModal({ incident, closeMapModal }: MapModalProps) {
   );
 }
 
-function Field({ label, value }: { label: string; value: any }) {
+function Field({ label, value }: { label: string; value: string | number }) {
   return (
     <div className="flex flex-col">
       <label className="text-sm font-medium text-gray-700 mb-1">{label}</label>
